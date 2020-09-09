4cf4aa7f10a166d0342a01773d87234df4399567
ZhangWeiWei <506997994@qq.com>
2020-09-09 19:15:16 +0800

    Add files via upload

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysMenu.java
@@ -0,0 +1,112 @@
+package com.ruoyi.system.domain;
+
+import com.ruoyi.common.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.ibatis.type.Alias;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 菜单权限表 sys_menu
+ *
+ * @author ruoyi
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Alias("SysMenu")
+public class SysMenu extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 菜单ID
+     */
+    private Long menuId;
+
+    /**
+     * 菜单名称
+     */
+    @NotBlank(message = "菜单名称不能为空")
+    @Size(max = 50, message = "菜单名称长度不能超过50个字符")
+    private String menuName;
+
+    /**
+     * 父菜单名称
+     */
+    private String parentName;
+
+    /**
+     * 父菜单ID
+     */
+    private Long parentId;
+
+    /**
+     * 显示顺序
+     */
+    @NotBlank(message = "显示顺序不能为空")
+    private String orderNum;
+
+    /**
+     * 菜单URL
+     */
+    @Size(max = 200, message = "请求地址不能超过200个字符")
+    private String url;
+
+    /**
+     * 打开方式：menuItem页签 menuBlank新窗口
+     */
+    private String target;
+
+    /**
+     * 类型:0目录,1菜单,2按钮
+     */
+    @NotBlank(message = "菜单类型不能为空")
+    private String menuType;
+
+    /**
+     * 菜单状态:0显示,1隐藏
+     */
+    private String visible;
+
+    /**
+     * 权限字符串
+     */
+    @Size(max = 100, message = "权限标识长度不能超过100个字符")
+    private String perms;
+
+    /**
+     * 菜单图标
+     */
+    private String icon;
+
+    /**
+     * 子菜单
+     */
+    private List<SysMenu> children = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("menuId" , getMenuId())
+                .append("menuName" , getMenuName())
+                .append("parentId" , getParentId())
+                .append("orderNum" , getOrderNum())
+                .append("url" , getUrl())
+                .append("target" , getTarget())
+                .append("menuType" , getMenuType())
+                .append("visible" , getVisible())
+                .append("perms" , getPerms())
+                .append("icon" , getIcon())
+                .append("createBy" , getCreateBy())
+                .append("createTime" , getCreateTime())
+                .append("updateBy" , getUpdateBy())
+                .append("updateTime" , getUpdateTime())
+                .append("remark" , getRemark())
+                .toString();
+    }
+}
